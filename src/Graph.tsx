7e22ea8b1e38473ff1adc5d6a27b5d84ceb46634
Shloaka Duvvuri <shloakaduvvuri@gmail.com>
2023-07-23 12:43:12 -0700

    Task 3: Display data visually for traders

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,19 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      // CHANGED schema to track ratio of 2 stock prices rather than individual stock price
+      // also added upper and lower bounds of the ratio to helo users determine trading opportunity
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float'
+      // stock: 'string',
+      // top_ask_price: 'float',
+      // top_bid_price: 'float',
+      // timestamp: 'date',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -34,25 +43,40 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
+      // CHANGED: removed column pivots because we care abour ratio not individual stock
+      // aggregates consolidates duplicate data into 1 data point
+      // a data point is unique if it has a timestamp so if it is not unique,
+      // we average the values of the other non-unique fiels
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      // elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      // elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      // elem.setAttribute('aggregates', JSON.stringify({
+      //   stock: 'distinctcount',
+      //   top_ask_price: 'avg',
+      //   top_bid_price: 'avg',
+      //   timestamp: 'distinct count',
+      // }));
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg'
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+        // CHANGED argument of this.table.update
+      ] as unknown as TableData);
     }
   }
 }
